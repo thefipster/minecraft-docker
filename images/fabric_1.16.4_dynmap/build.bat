91f815f5e2f5d0e94e5962c7fd6d39d0022bd345
Felix Reisch <felix.reisch.85@gmail.com>
2021-03-05 00:52:26 +0100

    Fixing sweep.

--- images/fabric_1.16.4_dynmap/build.bat
+++ images/fabric_1.16.4_dynmap/build.bat
@@ -1,3 +1,3 @@
-docker build -t minecraft/fabric_dynmap:1.16.4 .
+docker build -t thefipster/minecraft/fabric_dynmap:1.16.4 .
 rem docker tag minecraft/fabric_dynmap:1.16.4 registry.thefipster.com/minecraft/fabric_dynmap:1.16.4
 rem docker push registry.thefipster.com/minecraft/fabric_dynmap:1.16.4
